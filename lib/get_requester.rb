19b1236be8eba7b2d0761b152c2b40e53a728b29
+ <+>
2022-08-29 15:43:56 +0300

    commit tree 94b35f96e78d8bd6a46271262276d610de6d61fa

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,24 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester
+
+  URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+
+  def get_response_body
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+  def program_school
+    # we use the JSON library to parse the API response into nicely formatted JSON
+      programs = JSON.parse(self.get_response_body)
+      programs.collect do |program|
+        program["agency"]
+      end
+    end
+end
+
+programs = GetRequester.new.get_response_body
+puts programs
